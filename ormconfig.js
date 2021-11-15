db0d6eef76d2b493c56e10f5c34330c8adc94113
ldhnguyen92 <nguyen.le@hdwebsoft.co>
2021-11-15 08:52:44 +0700

    add migrationRun to ormconfig

--- ormconfig.js
+++ ormconfig.js
@@ -9,6 +9,7 @@ module.exports = {
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
   synchronize: true,
+  migrationsRun: true,
   logging: 'all',
   logger: 'file',
   entities: [entityDir],
